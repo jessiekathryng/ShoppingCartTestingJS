ddc6fd94ebc42a940336a7df23b549ce1075a92b
JessKG <jessiekathryng@gmail.com>
2022-08-03 23:36:41 -0500

    Passed Third Unit test.

--- ShopCartCode.js
+++ ShopCartCode.js
@@ -1,5 +1,9 @@
 // Testing module
 module.exports = class Checkout{
+    //Method for Test 2
     addItemPrice(item, price){
+    }
+    //Method for Test 3
+    addItem(item){
     }
 };
--- ShopCartTest.js
+++ ShopCartTest.js
@@ -1,7 +1,7 @@
 // 9 Test Cases
 //Canary Test - done
 //Can create an instance of the checkout class - done
-//Can add an item price
+//Can add an item price - done
 //Can add an item
 //Can calculate the current total
 //Can add multiple items and get correct total
@@ -12,13 +12,42 @@
 //This defines the variable 'Checkout'
 const Checkout = require('./ShopCartCode.js');
 var expect = require('chai').expect;
+
+beforeEach(function(){
+    checkout = new Checkout();
+});
+//Refactored: checkout declaration is duplicated in test 1 and 2;
+// Test 1 not necessary;
 //Test 1
-//Refactored: checkout expression is duplicated in test 1 and 2; Test 1 not necessary
 //it('Can create checkout', function(){
     //var checkout = new Checkout();
 //});
+
 //Test 2
+//Refactored: checkout declaration is duplicated; 
+//Added test setup function using 'beforeEach' to prevent duplication
+it('Can add an item price', function(){
+    checkout.addItemPrice('item', '1');
+});
+//Test 3
+it('Can add an item', function(){
+    var checkout = new Checkout();
+    checkout.addItemPrice('item', '1');
+    checkout.addItem('item');
+});
+
+
+/*
 it('Can add an item price', function(){
     var checkout = new Checkout();
     checkout.addItemPrice('a', '1');
 });
+it('Can add an item price', function(){
+    var checkout = new Checkout();
+    checkout.addItemPrice('a', '1');
+});
+it('Can add an item price', function(){
+    var checkout = new Checkout();
+    checkout.addItemPrice('a', '1');
+});
+*/
